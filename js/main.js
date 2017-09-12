93dcff347a47ce0c2c27321e460d37d031b8c53c
YuChing <haibara80143@gmail.com>
2017-09-12 08:25:56 +0800

    debug

--- js/main.js
+++ js/main.js
@@ -1298,7 +1298,7 @@ new Vue({
                     beeps.play();
                     $("#app").hide();
                     $("#score").hide();
-                    $("#final_score").text(score);
+                    $("#final_score_time").text(score);
                     $("#Q1,#Q2,#Q3,#Q4,#Q5,#Q6,#Q7,#Q8,#Q9,#Q10,#Q11,#Q12,#Q13,#Q14,#Q15,#Q16,#Q17,#Q18,#Q19,#Q20").hide();
                     $("#end_page_time").show();
                 }
